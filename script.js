5fb3b046f1f576110335685014fc9513fe3e1ea0
André Zana <andrezana@hotmail.com>
2025-09-16 16:21:40 -0300

    Acho que finalizei o projeto

--- script.js
+++ script.js
@@ -16,9 +16,24 @@ function toggleMode() {
   // Substituir a IMG
   if (html.classList.contains("light")) {
     // Se tiver light toggleMode, adiciona a IMG para light mode
-    img.setAttribute("src", "./assets/avatar_light_2.jpg")
+    img.setAttribute("src", "./assets/avatar_light.png")
   } else {
     // Se não tiver light toggleMode, manter a imagem para dark mode
     img.setAttribute("src", "./assets/avatar_az_2.png")
+  }
+
+  // Substituir o texto ALT da imagem
+  if (html.classList.contains("light")) {
+    // Se tiver light toggleMode, adiciona o texto descritivo referente a IMG para light mode
+    img.setAttribute(
+      "alt",
+      "Foto de André Zana feliz, sorridente, utilizando óculos de grau, camisa branca e gravata preta, com fundo colorido (numa festa)."
+    )
+  } else {
+    // Se não tiver light toggleMode, manter o texto descritivo referente a imagem para dark mode
+    img.setAttribute(
+      "alt",
+      "Foto de André Zana sério, utilizando óculos de grau e camiseta amarela, com fundo branco."
+    )
   }
 }
